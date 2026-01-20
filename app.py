cf6dde0c4b9c55b33f10983d1aa10d3f53c337cc
Maximilian Rödiger <maxroediger@googlemail.com>
2026-01-20 14:03:32 +0100

    Added templates and static dirs with index.html and style.css. Created default route '/' in app.py, that parses the json data to the template

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import json
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    with open('blog_posts.json', 'r') as handle:
+        blog_posts = json.load(handle)
+    return render_template('index.html', posts=blog_posts)
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
